d80f8e06166878af251eaa306e55659e4ac4c8fd
Cosam Crasto <cosamcrasto08@gmail.com>
2025-08-24 14:24:55 +0530

    Add files via upload

---
+++ player.py
@@ -0,0 +1,22 @@
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+from turtle import Turtle
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("black")
+        self.penup()
+        self.tilt(90)
+        self.goto(STARTING_POSITION)
+
+    def move_up(self):
+        new_y=self.ycor()+MOVE_DISTANCE
+        self.goto(self.xcor(),new_y)
+
+    def finish_line(self):
+
+        self.goto(STARTING_POSITION)
+
